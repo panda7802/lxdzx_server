a6365fbfb3ec309ff898a035ea18ec1a57f796fe
panda7802 <panguotian@gmail.com>
2017-11-17 15:58:23 +0800

    add log

---
+++ tutils/tconf.py
@@ -0,0 +1,20 @@
+# coding=utf8
+
+'''
+Created on 2016年12月20日
+
+ 测试环境的配置
+
+@author: pangt
+'''
+from os.path import os
+
+PRO_TYPE_DEV = "PRO_TYPE_DEV"
+PRO_TYPE_PRD = "PRO_TYPE_PRD"
+PRO_TYPE = PRO_TYPE_PRD
+
+
+T_SUCCESS = 0
+T_FAILED = 1
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- video_manager/views.py
+++ video_manager/views.py
@@ -8,20 +8,27 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+import logging
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 # 转发URL
+
 def trans(request, url):
     s = ""
     try:
-        print "url:", url
-        #解码两遍URL
+        # print "url:", url
+        # 解码两遍URL
         url = urllib.unquote(url)
-        url = urllib.unquote(url)
+        logging.error("url1 : ----------------" + url)
+        # url = urllib.unquote(url)
+        # logging.info("url2 : " + url)
         s = urllib2.urlopen(url).read()
     except Exception, e:
         s = "转发URL异常:" + url
-#        print e
-        s = str(e)
+        print e
     finally:
         return HttpResponse(s)
-
